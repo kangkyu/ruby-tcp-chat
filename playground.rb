786247c1f87f2775cc6d2cd4ce196c4354e10709
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-08-14 01:09:21 -0700

    Add read/puts request info
    'curl: (56) Recv failure: Connection reset by peer' error is gone
    for some reason

--- playground.rb
+++ playground.rb
@@ -4,6 +4,9 @@ server = TCPServer.new(3000)
 puts "Listening on port #{server.local_address.ip_port}"
 
 Socket.accept_loop(server) do |socket|
+  data = socket.readpartial(1024)
+  puts data
+
   socket.write "hello\n"
 
   socket.close
